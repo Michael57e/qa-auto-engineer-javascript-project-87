2fe74de01f60c3c9b01999af4f3bdd986c09422c
Michael <misha9275@gmail.com>
2025-12-14 21:06:55 +0100

    fixed src/gendiff.js from the previous step

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,43 +1,44 @@
 import fs from 'fs'
-//import path from 'path' // что это? можно убрать
+// import path from 'path' // что это? можно убрать
 import stylish from './stylish.js'
 
 const parseJSON = (filepath) => {
   const content = fs.readFileSync(filepath, 'utf8')
   return JSON.parse(content)
-};
+}
 
 export const genDiff = (filepath1, filepath2) => {
   const obj1 = parseJSON(filepath1)
   const obj2 = parseJSON(filepath2)
 
-  const keys = Array.from(new Set([...Object.keys(obj1), ...Object.keys(obj2)]))
-    .sort()
-
   const diff = {}
 
-  keys.forEach((key) => {
-    if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
+  for (const key of Object.keys(obj1)) {
+    if (!Object.hasOwn(obj2, key)) {
       diff[key] = { type: 'removed', value: obj1[key] }
-      return
-    }
-
-    if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      diff[key] = { type: 'added', value: obj2[key] }
-      return
-    }
-
-    if (Object.is(obj1[key], obj2[key])) {
+    } else if (Object.is(obj1[key], obj2[key])) {
       diff[key] = { type: 'unchanged', value: obj1[key] }
-      return
+    } else {
+      diff[key] = {
+        type: 'changed',
+        oldValue: obj1[key],
+        newValue: obj2[key],
+      }
     }
+  }
 
-    diff[key] = {
-      type: 'changed',
-      oldValue: obj1[key],
-      newValue: obj2[key],
+  for (const key of Object.keys(obj2)) {
+    if (!Object.hasOwn(obj1, key)) {
+      diff[key] = { type: 'added', value: obj2[key] }
     }
-  })
+  }
 
-  return stylish(diff)
+  const sortedDiff = Object.keys(diff)
+    .sort()
+    .reduce((acc, key) => {
+      acc[key] = diff[key]
+      return acc
+    }, {})
+
+  return stylish(sortedDiff)
 }
