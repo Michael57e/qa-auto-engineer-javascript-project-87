33dc1aedcb48bfce8788b2da52eaba94c9a76cf3
Michael <misha9275@gmail.com>
2026-02-08 14:33:43 +0100

    modified test files x3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,6 +1,10 @@
 import path from 'path'
 import fs from 'fs'
+import { fileURLToPath } from 'url'
 import gendiff from '../src/index.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const getFixturePath = filename =>
   path.join(__dirname, '..', '__fixtures__', filename)
